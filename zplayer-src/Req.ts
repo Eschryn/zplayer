c0ab195f9532c801ae2d6d1d734aff0e864dafda
Eschryn <8217655+Eschryn@users.noreply.github.com>
2019-10-26 18:58:08 +0200

    Remove unused files

--- zplayer-src/Req.ts
+++
@@ -1,9 +0,0 @@
-class Req {
-    public static ArrayBuffer(URI: string, receive: (ev: ArrayBuffer) => any): any {
-        let req = new XMLHttpRequest();
-        req.open("GET", URI, false);
-        req.responseType = "arraybuffer";
-        req.send();
-        return receive(req.response);
-    }
-}
